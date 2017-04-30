98b34953717ddfb5cb24a09fffd38d45713109b2
Alex <kik9399@mail.ru>
2017-04-30 14:45:07 +0300

    fix #2

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,9 +2,9 @@
 #include <catch.hpp>
 
 SCENARIO ("constructorTest"){
-	Complex comp(20,13);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	Complex comp1(20,13);
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 20);
 	REQUIRE (Imag == 13);
 }
@@ -13,8 +13,8 @@ SCENARIO ("Addition"){
 	Complex comp1(20,13);
 	Complex comp2(12,14);
 	Comp1.add(comp2);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 32);
 	REQUIRE (Imag == 27);
 }
@@ -23,8 +23,8 @@ SCENARIO ("Sub"){
 	Complex comp1(20,13);
 	Complex comp2(12,14);
 	Comp1.sub(comp2);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 8);
 	REQUIRE (Imag == 1);
 }
@@ -32,8 +32,8 @@ SCENARIO ("Sub"){
 SCENARIO ("MultiNum"){
 	Complex comp1(20,13);
 	Comp1.mult(2);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 40);
 	REQUIRE (Imag == 26);
 }
@@ -41,8 +41,8 @@ SCENARIO ("MultiNum"){
 SCENARIO ("DivNum"){
 	Complex comp1(20,14);
 	Comp1.div(2);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 10);
 	REQUIRE (Imag == 7);
 }
@@ -50,8 +50,8 @@ SCENARIO ("DivNum"){
 SCENARIO ("DivNumZero"){
 	Complex comp1(20,13);
 	Comp1.div(0);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 20);
 	REQUIRE (Imag == 13);
 }
@@ -60,8 +60,8 @@ SCENARIO("MultiComplex"){
 	Complex comp1(20,13);
 	Complex comp2(12,14);
 	Comp1.multi_complex(comp2);
-	Real = comp.getReal();
-	Imag = comp.getImag();
+	double Real = comp1.getReal();
+	double Imag = comp1.getImag();
 	REQUIRE (Real == 58);
 	REQUIRE (Imag == 436);
 }
