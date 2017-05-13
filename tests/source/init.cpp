2b0716c648c90f8bf1e6a47d47e386f7c7c903ae
Alex <kik9399@mail.ru>
2017-05-14 01:42:20 +0300

    v1.2 beta2

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -6,56 +6,56 @@ Complex<int> y { 2 , 5 };
 Complex<int> comp{};
 
 SCENARIO ("constructorTest"){
-	double Real = x.getReal();
-	double Imag = x.getIm();
+	int Real = x.getReal();
+	int Imag = x.getIm();
 	REQUIRE (Real == 10);
 	REQUIRE (Imag == 15);
 }
 
 SCENARIO ("Addition"){
 	comp.add(x,y);
-	double Real = comp.getReal();
-	double Imag = comp.getIm();
+	int Real = comp.getReal();
+	int Imag = comp.getIm();
 	REQUIRE (Real == 12);
 	REQUIRE (Imag == 20);
 }
 
 SCENARIO ("Sub"){
 	comp.sub(x,y);
-	double Real = comp.getReal();
-	double Imag = comp.getIm();
+	int Real = comp.getReal();
+	int Imag = comp.getIm();
 	REQUIRE (Real == 8);
 	REQUIRE (Imag == 10);
 }
 
 SCENARIO ("MultiNum"){
-	comp.mult(x,2)
-	double Real = comp.getReal();
-	double Imag = comp.getIm();
+	comp.mult(x,2);
+	int Real = comp.getReal();
+	int Imag = comp.getIm();
 	REQUIRE (Real == 20);
 	REQUIRE (Imag == 30);
 }
 
 SCENARIO ("DivNum"){
 	comp.div(x,5);
-	double Real = comp.getReal();
-	double Imag = comp.getIm();
+	int Real = comp.getReal();
+	int Imag = comp.getIm();
 	REQUIRE (Real == 2);
 	REQUIRE (Imag == 3);
 }
 
 SCENARIO ("DivNumZero","[!mayfail]"){
 	comp.div(x,0);
-	double Real = comp.getReal();
-	double Imag = comp.getIm();
+	int Real = comp.getReal();
+	int Imag = comp.getIm();
 	REQUIRE (Real == 0);
 	REQUIRE (Imag == 0);
 }
 
 SCENARIO("MultiComplex"){
 	comp.multC(x,y)
-	double Real = comp.getReal();
-	double Imag = comp.getIm();
+	int Real = comp.getReal();
+	int Imag = comp.getIm();
 	REQUIRE (Real == -55);
 	REQUIRE (Imag == 80);
 }
